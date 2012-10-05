9d693af4c60b1fde9735718542fb7d96cc82437e
Jonathan C Cast <jcast@globalscript.org>
2012-10-05 01:40:57 +0000

    2012-10-04 Set up IBIO to use the API system

--- src/cmd/ibio/ibio.c
+++ src/cmd/ibio/ibio.c
@@ -10,8 +10,21 @@ p9main(int argc, char **argv)
     gsmain(argc, argv);
 }
 
+enum {
+    ibio_numrpcs = api_std_rpc_numrpcs,
+};
+
+static struct api_process_rpc_table exec_table = {
+    /* name = */ "IOLL Unix pool process",
+    /* numrpcs = */ ibio_numrpcs,
+    /* rpcs = */ {
+        /* api_std_rpc_done */ api_main_process_unimpl_rpc,
+        /* api_std_rpc_abend */ api_main_process_handle_rpc_abend,
+    },
+};
+
 void
 gsrun(gsvalue prog)
 {
-    gsfatal_unimpl(__FILE__, __LINE__, "gsrun");
+    apisetupmainthread(&exec_table, prog);
 }
