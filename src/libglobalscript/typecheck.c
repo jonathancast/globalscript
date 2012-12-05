aed316e231c9d24abd2d53d0c33f9572ee5529cd
Jonathan C Cast <jcast@globalscript.org>
2012-12-05 21:34:32 +0000

    2012-12-05 Print defined primtypes

--- src/libglobalscript/typecheck.c
+++ src/libglobalscript/typecheck.c
@@ -3051,6 +3051,9 @@ gstypes_eprint_type(char *res, char *eob, struct gstype *pty)
             prim = (struct gstype_unprim *)pty;
 
             switch (prim->primtypegroup) {
+                case gsprim_type_defined:
+                    res = seprint(res, eob, "\"definedprim ");
+                    break;
                 case gsprim_type_elim:
                     res = seprint(res, eob, "\"elimprim ");
                     break;
