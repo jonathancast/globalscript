2da2f8e4b3d9893325304984ffb103b408bfc02f
Jonathan C Cast <jcast@globalscript.org>
2013-02-26 20:03:39 +0000

    [gc] Simplify gsnaturaltochar to just use seprint

--- src/libglobalscript/naturals.c
+++ src/libglobalscript/naturals.c
@@ -211,9 +211,6 @@ natural_prim_handle_gt(struct ace_thread *thread, struct gspos pos, int nargs, g
 char *
 gsnaturaltochar(char *err, char *eerr, gsvalue v, char *buf, char *ebuf)
 {
-    int len;
-    gsvalue ubound;
-
     if (IS_PTR(v)) {
         seprint(err, eerr, "Cannot print %p; is a pointer", v);
         return 0;
@@ -221,24 +218,5 @@ gsnaturaltochar(char *err, char *eerr, gsvalue v, char *buf, char *ebuf)
 
     v &= ~GS_MAX_PTR;
 
-    len = 1;
-    ubound = 10;
-
-    while (ubound < GS_MAX_PTR / 10 && ubound <= v)
-        len++, ubound *= 10
-    ;
-
-    if (ebuf - buf - 1 < len) {
-        seprint(err, eerr, "Cannot print %p; has %d digits but only have space for %d", len, ebuf - buf - 1);
-        return 0;
-    }
-
-    while (len--) {
-        ubound = ubound / 10;
-        *buf++ = '0' + v % ubound;
-    }
-
-    *buf++ = 0;
-
-    return buf;
+    return seprint(buf, ebuf, "%ud", v);
 }
