a220e63a591e02a9a49f88ad081fef41095bfc78
Jonathan Cast <jcast@globalscript.org>
2013-03-11 05:02:51 +0000

    [cleanup] Fix indentation

--- src/libglobalscript/unix/iosysconstants.c
+++ src/libglobalscript/unix/iosysconstants.c
@@ -9,10 +9,10 @@ char *
 gsbio_efmt_iochannel_type(char *result, char *end, enum gsbio_iochannel_type ty)
 {
     switch (ty) {
-    case gsbio_iostat:
-        return seprint(result, end, "gsbio_iostat");
-    default:
-        return seprint(result, end, "'Unk iochannel type': %d", ty);
+        case gsbio_iostat:
+            return seprint(result, end, "gsbio_iostat");
+        default:
+            return seprint(result, end, "'Unk iochannel type': %d", ty);
     }
 }
 
