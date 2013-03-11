6abe129f1e3ec18dc402aa8d74dd35f933a93e95
Jonathan Cast <jcast@globalscript.org>
2013-03-11 05:11:41 +0000

    [cleanup] I can't see the difference between clienttypes.c and clienttypecheck.c, so merge them

--- src/libglobalscript/clienttypecheck.c
+++ src/libglobalscript/clienttypecheck.c
@@ -293,3 +293,18 @@ gstype_get_definition(struct gspos pos, struct gsfile_symtable *symtable, struct
         return 0;
     }
 }
+
+struct gstype *
+gstypes_compile_list(struct gspos pos, struct gstype *ty)
+{
+    struct gstype *list;
+
+    list = gstypes_compile_abstract(pos, gsintern_string(gssymtypelable, "list.t"), gskind_compile_string(pos, "**^"));
+    return gstype_apply(pos, list, ty);
+}
+
+struct gstype *
+gstypes_compile_rune(struct gspos pos)
+{
+    return gstypes_compile_abstract(pos, gsintern_string(gssymtypelable, "rune.t"), gskind_compile_string(pos, "*"));
+}
