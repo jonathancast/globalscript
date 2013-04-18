9a2b6a07db1f24ead52836d943b55a464b910bf3
Jonathan C Cast <jcast@globalscript.org>
2013-04-18 02:48:09 +0000

    [ibio:1] Test file for iport.c

--- src/cmd/ibio/test.c
+++ src/cmd/ibio/test.c
@@ -11,6 +11,7 @@ p9main(int argc, char **argv)
 {
     argv0 = *argv;
     start_tests();
+    test_iport();
     run_system_tests();
     exits("");
 }
---
+++ src/cmd/ibio/test_iport.c
@@ -0,0 +1,14 @@
+#include <u.h>
+#include <libc.h>
+#include <libtest.h>
+#include <libglobalscript.h>
+
+#include "test_tests.h"
+
+#include "ibio.h"
+#include "iport.h"
+
+void
+test_iport()
+{
+}
