f8d7d9ef0e48669d075417d78389b9fde06b2994
Jonathan C Cast <jcast@globalscript.org>
2013-06-04 03:19:58 +0000

    [bc] Don't use phase to control the location of .tygvars, but rather the structure of the byte-compilation code

--- src/libglobalscript/bytecompile.c
+++ src/libglobalscript/bytecompile.c
@@ -1402,7 +1402,6 @@ gsbc_bytecompile_code_item(struct gsfile_symtable *symtable, struct gsparsedfile
 
 struct gsbc_byte_compile_code_or_api_op_closure {
     enum {
-        rttygvars,
         rttyfvs,
         rttyargs,
         rttylets,
@@ -1468,9 +1467,9 @@ gsbc_byte_compile_code_ops(struct gsfile_symtable *symtable, struct gsparsedfile
 
     gsbc_byte_compile_code_or_api_op_closure_init(pbco, &cl);
     pcl = &cl;
+    while (gsbc_byte_compile_type_gvar_code_op(symtable, p, &cl)) p = gsinput_next_line(ppseg, p);
     for (; ; p = gsinput_next_line(ppseg, p)) {
-        if (gsbc_byte_compile_type_gvar_code_op(symtable, p, &cl)) {
-        } else if (gsbc_byte_compile_type_fv_code_op(symtable, p, &cl)) {
+        if (gsbc_byte_compile_type_fv_code_op(symtable, p, &cl)) {
         } else if (gsbc_byte_compile_type_arg_code_op(p, &cl)) {
         } else if (gsbc_byte_compile_type_let_code_op(p, &cl)) {
         } else if (gsbc_byte_compile_data_fv_code_op(symtable, p, &cl)) {
@@ -1557,7 +1556,7 @@ static
 void
 gsbc_byte_compile_code_or_api_op_closure_init(struct gsbco *pbco, struct gsbc_byte_compile_code_or_api_op_closure *pcl)
 {
-    pcl->phase = rttygvars;
+    pcl->phase = rttyfvs;
     pcl->ntyregs = pcl->nregs = pcl->nsubexprs = pcl->nglobals = pcl->nfvs = pcl->nargs = pcl->nfields = 0;
     pcl->pout = (uchar*)pbco + sizeof(struct gsbco);
     memset(pcl->regtypes, 0, sizeof(pcl->regtypes));
@@ -1589,10 +1588,6 @@ int
 gsbc_byte_compile_type_gvar_code_op(struct gsfile_symtable *symtable, struct gsparsedline *p, struct gsbc_byte_compile_code_or_api_op_closure *pcl)
 {
     if (gssymceq(p->directive, gssymoptygvar, gssymcodeop, ".tygvar")) {
-        if (pcl->phase > rttygvars)
-            gsfatal("%P: Too late to add type global variables", p->pos)
-        ;
-        pcl->phase = rttygvars;
         if (pcl->ntyregs >= MAX_NUM_REGISTERS)
             gsfatal("%P: Too many type registers", p->pos)
         ;
@@ -2497,9 +2492,9 @@ gsbc_byte_compile_api_ops(struct gsfile_symtable *symtable, struct gsparsedfile_
     struct gsbc *pcode;
 
     gsbc_byte_compile_code_or_api_op_closure_init(pbco, &cl);
+    while (gsbc_byte_compile_type_gvar_code_op(symtable, p, &cl)) p = gsinput_next_line(ppseg, p);
     for (; ; p = gsinput_next_line(ppseg, p)) {
-        if (gsbc_byte_compile_type_gvar_code_op(symtable, p, &cl)) {
-        } else if (gsbc_byte_compile_type_fv_code_op(symtable, p, &cl)) {
+        if (gsbc_byte_compile_type_fv_code_op(symtable, p, &cl)) {
         } else if (gsbc_byte_compile_type_arg_code_op(p, &cl)) {
         } else if (gsbc_byte_compile_data_fv_code_op(symtable, p, &cl)) {
         } else if (gssymceq(p->directive, gssymopsubcode, gssymcodeop, ".subcode")) {
