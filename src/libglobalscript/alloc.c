d9f86e14c283ff76d9e3bc0d9fa5380eb7402c5c
Jonathan C Cast <jcast@globalscript.org>
2014-07-13 23:18:14 +0000

    [gc] Switch from a small array of contiguous segments to a large array of individual blocks

--- src/libglobalscript/alloc.c
+++ src/libglobalscript/alloc.c
@@ -7,18 +7,27 @@
 
 Lock gs_allocator_lock;
 
-#define GS_SYS_MAX_NUM_SEGMENTS 0x10
+enum gs_sys_block_type {
+    gs_sys_block_break,
+    gs_sys_block_free,
+    gs_sys_block_allocated,
+    gs_sys_block_pinned,
+    gs_sys_block_not_ours,
+    gs_sys_block_gc_from_space,
+};
 
-static int gs_sys_num_segments;
-static struct gs_sys_segment {
-    enum {
-        gs_sys_segment_break,
-        gs_sys_segment_free,
-        gs_sys_segment_allocated,
-        gs_sys_segment_gc_from_space,
-    } type;
-    void *base;
-} gs_sys_segments[GS_SYS_MAX_NUM_SEGMENTS];
+static enum gs_sys_block_type *blocks;
+static void *bottom_of_data;
+static void *what_we_think_is_break;
+
+#define MAX_NUM_LEGAL_BLOCKS (BLOCK_SIZE / sizeof(*blocks))
+#define MAX_NUM_AVAILABLE_BLOCKS (BLOCK_SIZE / ((uchar*)GS_MAX_PTR - (uchar*)bottom_of_data)
+#define BLOCK_TYPE(p) (blocks[(uintptr)BLOCK_CONTAINING(p) / BLOCK_SIZE])
+
+#define BLOCK_AT_INDEX(i) ((void*)(i * BLOCK_SIZE))
+#define FIRST_BLOCK_INDEX ((uintptr)bottom_of_data / BLOCK_SIZE)
+#define LAST_BLOCK_INDEX ((uintptr)what_we_think_is_break / BLOCK_SIZE)
+
 /* OK, this is confusing.
    §ccode{gs_sys_gc_running} means main process is somewhere in the GC code.
    §ccode{gs_sys_in_gc} means there exist 'from space' segments.
@@ -31,30 +40,27 @@ static int gs_sys_in_gc;
 void
 gs_sys_memory_init(void)
 {
-    void *bottom_of_data;
+    int i = 0;
+
+    if (blocks) gsfatal("gs_sys_memory_init called twice");
 
     bottom_of_data = sbrk(0);
     if ((uintptr)bottom_of_data % BLOCK_SIZE) {
         bottom_of_data = (uchar*)bottom_of_data + BLOCK_SIZE - ((uintptr)bottom_of_data % BLOCK_SIZE);
-        if (brk(bottom_of_data) < 0)
-            gsfatal("brk failed! %r")
-        ;
+        if (brk(bottom_of_data) < 0) gsfatal("brk failed! %r");
     }
     if ((uintptr)bottom_of_data >= GS_MAX_PTR)
         gsfatal("Out of memory in gs_sys_memory_init")
     ;
+    blocks = bottom_of_data;
 
-    gs_sys_num_segments = 1;
-    gs_sys_segments[0].type = gs_sys_segment_break;
-    gs_sys_segments[0].base = bottom_of_data;
+    bottom_of_data = (uchar*)bottom_of_data + BLOCK_SIZE;
+    if (brk(bottom_of_data) < 0) gsfatal("brk failed! %r");
+
+    what_we_think_is_break = bottom_of_data;
+
+    for (i = 0; i < FIRST_BLOCK_INDEX; i++) blocks[i] = gs_sys_block_not_ours;
 }
-
-#define GS_SYS_SEGMENT_TOP(i) ((i) < gs_sys_num_segments ? gs_sys_segments[(i) + 1].base : (void*)GS_MAX_PTR)
-
-#define GS_SYS_SEGMENT_SIZE(i) ((uchar*)GS_SYS_SEGMENT_TOP(i) - (uchar*)gs_sys_segments[i].base)
-
-#define GS_SYS_SEGMENT_IS(i, ty) \
-    ((i) >= 0 && (i) < gs_sys_num_segments && gs_sys_segments[i].type == ty)
 
 gsumemorysize
 gs_sys_memory_allocated_size()
@@ -63,9 +69,8 @@ gs_sys_memory_allocated_size()
     int i;
 
     res = 0;
-    for (i = 0; i < gs_sys_num_segments; i++)
-        if (GS_SYS_SEGMENT_IS(i, gs_sys_segment_allocated))
-            res += GS_SYS_SEGMENT_SIZE(i)
+    for (i = FIRST_BLOCK_INDEX; i < LAST_BLOCK_INDEX; i++)
+        if (blocks[i] == gs_sys_block_allocated) res += BLOCK_SIZE
     ;
 
     return res;
@@ -78,7 +83,7 @@ gs_sys_memory_exhausted()
 
     lock(&gs_allocator_lock);
     res =
-        gs_sys_segments[gs_sys_num_segments - 1].type != gs_sys_segment_break
+        (uintptr)what_we_think_is_break >= GS_MAX_PTR
         || gs_sys_memory_allocated_size() >= 0x200 * 0x400 * 0x400
     ;
     unlock(&gs_allocator_lock);
@@ -133,9 +138,9 @@ gs_sys_start_gc(struct gsstringbuilder *err)
 
     for (i = 0; i < gs_sys_gc_num_pre_callbacks; i++) gs_sys_gc_pre_callbacks[i]();
 
-    for (i = 0; i < gs_sys_num_segments; i++) {
-        if (gs_sys_segments[i].type == gs_sys_segment_allocated)
-            gs_sys_segments[i].type = gs_sys_segment_gc_from_space
+    for (i = FIRST_BLOCK_INDEX; i < LAST_BLOCK_INDEX; i++) {
+        if (blocks[i] == gs_sys_block_allocated)
+            blocks[i] = gs_sys_block_gc_from_space
         ;
     }
 
@@ -159,14 +164,6 @@ gs_gc_trace_pos(struct gsstringbuilder *err, struct gspos *pos)
     return 0;
 }
 
-#define GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, nsegs) \
-    do { \
-        for (j = i; j + nsegs < gs_sys_num_segments; j++) \
-            gs_sys_segments[j] = gs_sys_segments[j + nsegs] \
-        ; \
-        gs_sys_num_segments -= nsegs; \
-    } while (0)
-
 #define GS_SYS_GC_MAX_NUM_POST_CALLBACKS 0x100
 static int gs_sys_gc_num_post_callbacks;
 static gs_sys_gc_post_callback *gs_sys_gc_post_callbacks[GS_SYS_GC_MAX_NUM_POST_CALLBACKS];
@@ -174,29 +171,15 @@ static gs_sys_gc_post_callback *gs_sys_gc_post_callbacks[GS_SYS_GC_MAX_NUM_POST_
 int
 gs_sys_finish_gc(struct gsstringbuilder *err)
 {
-    int i, j;
+    int i;
 
     for (i = 0; i < gs_sys_gc_num_post_callbacks; i++)
         if (gs_sys_gc_post_callbacks[i](err) < 0)
             return -1
     ;
 
-    for (i = 0; i < gs_sys_num_segments;) {
-        if (gs_sys_segments[i].type == gs_sys_segment_gc_from_space) {
-            if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_free) && GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_free)) {
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 2);
-                i --;
-            } else if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_free)) {
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 1);
-            } else if (GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_free)) {
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i + 1, 1);
-            } else {
-                gs_sys_segments[i].type = gs_sys_segment_free;
-                i++;
-            }
-        } else {
-            i++;
-        }
+    for (i = FIRST_BLOCK_INDEX; i < LAST_BLOCK_INDEX; i++) {
+        if (blocks[i] == gs_sys_block_gc_from_space) blocks[i] = gs_sys_block_free;
     }
 
     gs_sys_in_gc = 0;
@@ -230,29 +213,14 @@ static gs_sys_gc_failure_callback *gs_sys_gc_failure_callbacks[GS_SYS_GC_MAX_NUM
 void
 gs_sys_gc_failed(char *err)
 {
-    int i, j;
+    int i;
 
     for (i = 0; i < gs_sys_gc_num_failure_callbacks; i++)
         gs_sys_gc_failure_callbacks[i]()
     ;
 
-    for (i = 0; i < gs_sys_num_segments;) {
-        if (gs_sys_segments[i].type == gs_sys_segment_gc_from_space) {
-            if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_allocated) && GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_allocated)) {
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 2);
-                i--;
-            } else if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_allocated)) {
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 1);
-            } else if (GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_allocated)) {
-                gs_sys_segments[i].type = gs_sys_segment_allocated;
-                GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i + 1, 1);
-            } else {
-                gs_sys_segments[i].type = gs_sys_segment_allocated;
-                i++;
-            }
-        } else {
-            i++;
-        }
+    for (i = FIRST_BLOCK_INDEX; i < LAST_BLOCK_INDEX; i++) {
+        if (blocks[i] == gs_sys_block_gc_from_space) blocks[i] = gs_sys_block_allocated;
     }
 
     gs_sys_in_gc = 0;
@@ -337,19 +305,9 @@ gs_sys_gc_allow_collection(struct gsstringbuilder *err)
 int
 gs_sys_block_in_gc_from_space(void *p)
 {
-    int i;
-
     if (!gs_sys_in_gc) return 0;
-    for (i = 0; i < gs_sys_num_segments; i++) {
-        if (
-            gs_sys_segments[i].type == gs_sys_segment_gc_from_space
-            && (uchar*)gs_sys_segments[i].base <= (uchar*)p
-            && (uchar*)p < (uchar*)GS_SYS_SEGMENT_TOP(i)
-        )
-            return 1
-        ;
-    }
-    return 0;
+    if ((uintptr)p >= (uintptr)what_we_think_is_break) return 0;
+    return BLOCK_TYPE(p) == gs_sys_block_gc_from_space;
 }
 
 void
@@ -388,20 +346,6 @@ gs_sys_gc_failure_callback_register(gs_sys_gc_failure_callback *cb)
     gs_sys_gc_failure_callbacks[gs_sys_gc_num_failure_callbacks++] = cb;
 }
 
-#define GS_SPLIT_SEGMENT(i, ty, sz) \
-    do { \
-        if (gs_sys_num_segments >= GS_SYS_MAX_NUM_SEGMENTS) \
-            gswarning("%s:%d: Things are about to break b/c we're out of segments", __FILE__, __LINE__) \
-        ; \
-        for (j = gs_sys_num_segments; j > i; j--) \
-            gs_sys_segments[j] = gs_sys_segments[j - 1] \
-        ; \
-        gs_sys_segments[i + 1].type = gs_sys_segments[i].type; \
-        gs_sys_segments[i + 1].base = (uchar*)gs_sys_segments[i].base + sz; \
-        gs_sys_segments[i].type = ty; \
-        gs_sys_num_segments++; \
-    } while (0)
-
 /* Needs to lock the program break */
 void *
 gs_sys_block_alloc(registered_block_class cl)
@@ -411,64 +355,39 @@ gs_sys_block_alloc(registered_block_class cl)
 
     lock(&gs_allocator_lock);
 
-    if (!gs_sys_num_segments)
-        gs_sys_memory_init()
-    ;
+    if (!blocks) gs_sys_memory_init();
 
-    for (i = 0; i < gs_sys_num_segments; i++) {
-        switch (gs_sys_segments[i].type) {
-            case gs_sys_segment_break: {
-                gsumemorysize old_memory_size, break_size, new_free_segment_size;
-
-                old_memory_size = (uchar*)gs_sys_segments[i].base - (uchar*)gs_sys_segments[0].base;
-                break_size = (uchar*)GS_MAX_PTR - (uchar*)gs_sys_segments[i].base;
-
-                new_free_segment_size = old_memory_size;
-                if (new_free_segment_size == 0) new_free_segment_size = BLOCK_SIZE;
-                if (new_free_segment_size >= break_size) new_free_segment_size = break_size;
-                if (brk((uchar*)gs_sys_segments[i].base + new_free_segment_size) < 0)
-                    gswarning("%s:%d: Could not extend data segment: brk failed (allocated %p to %p, allocating %p)! %r", __FILE__, __LINE__, gs_sys_segments[0].base, gs_sys_segments[i].base, new_free_segment_size)
-                ;
-                if (new_free_segment_size < break_size) {
-                    GS_SPLIT_SEGMENT(i, gs_sys_segment_free, new_free_segment_size);
-                } else {
-                    gswarning("%s:%d: Extended program break to maximum location", __FILE__, __LINE__);
-                    gs_sys_segments[i].type = gs_sys_segment_free;
-                }
-            }
-            /* Deliberately falling through; §ccode{i} needs to point at the §emph{free} segment we just created now */
-            case gs_sys_segment_free:
-                pres = (struct gs_blockdesc *)gs_sys_segments[i].base;
-                if (GS_SYS_SEGMENT_SIZE(i) > BLOCK_SIZE) {
-                    if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_allocated)) {
-                        gs_sys_segments[i].base = (uchar*)gs_sys_segments[i].base + BLOCK_SIZE;
-                    } else {
-                        GS_SPLIT_SEGMENT(i, gs_sys_segment_allocated, BLOCK_SIZE);
-                    }
-                } else {
-                    if (
-                        GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_allocated)
-                        && GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_allocated)
-                    ) {
-                        GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 2);
-                    } else if (GS_SYS_SEGMENT_IS(i - 1, gs_sys_segment_allocated)) {
-                        GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i, 1);
-                    } else if (GS_SYS_SEGMENT_IS(i + 1, gs_sys_segment_allocated)) {
-                        gs_sys_segments[i].type = gs_sys_segment_allocated;
-                        GS_SYS_COALESCE_WITH_PREVIOUS_SEGMENT(i + 1, 1);
-                    } else {
-                        /* §paragraph{Just mark this segment allocated} */
-                        gs_sys_segments[i].type = gs_sys_segment_allocated;
-                    }
-                }
-                goto have_pres;
-        }
+    /* Try to find a free block (TODO: linear scan can probably be improved) */
+    for (i = FIRST_BLOCK_INDEX; i < LAST_BLOCK_INDEX; i++) {
+        if (blocks[i] == gs_sys_block_free) break;
     }
-    gswarning("%s:%d: About to stop because we're out of memory", __FILE__, __LINE__);
-    pres = 0;
 
-have_pres:
+    /* If we don't have a free block, try to extend available memory */
+    if (i == LAST_BLOCK_INDEX) {
+        gsumemorysize old_memory_size, break_size, new_free_segment_size;
+        void *actual_break;
 
+        actual_break = sbrk(0);
+        if ((uintptr)actual_break > (uintptr)what_we_think_is_break) {
+            gswarning(UNIMPL("Break moved when we weren't looking at it, from %p to %p; proceeding as if it never happened"), what_we_think_is_break, actual_break);
+        }
+
+        old_memory_size = (uchar*)what_we_think_is_break - (uchar*)bottom_of_data;
+        break_size = (uchar*)GS_MAX_PTR - (uchar*)what_we_think_is_break;
+        new_free_segment_size = old_memory_size;
+        
+        if (break_size <= 0) gswarning(UNIMPL("We've reached gs_sys_block_alloc even though memory is exhausted"));
+        if (new_free_segment_size == 0) new_free_segment_size = BLOCK_SIZE;
+        if (new_free_segment_size >= break_size) new_free_segment_size = break_size;
+        what_we_think_is_break = (uchar*)what_we_think_is_break + new_free_segment_size;
+        if (brk(what_we_think_is_break) < 0)
+            gswarning("Could not extend data segment: brk failed (allocated %p to %p, allocating %p)! %r", bottom_of_data, what_we_think_is_break, new_free_segment_size)
+        ;
+        for (j = i; j < LAST_BLOCK_INDEX; j++) blocks[j] = gs_sys_block_free;
+    }
+
+    blocks[i] = gs_sys_block_allocated;
+    pres = i < LAST_BLOCK_INDEX ? BLOCK_AT_INDEX(i) : 0;
     pres->class = cl;
 
     unlock(&gs_allocator_lock);
