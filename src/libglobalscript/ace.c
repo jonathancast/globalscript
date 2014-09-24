37640196e795bb02e2bb0413c0bb203191a1c1c4
Jonathan C Cast <jcast@globalscript.org>
2014-09-24 02:25:15 +0000

    [ace] Execute all the .efv instructions at once in an inner loop

--- src/libglobalscript/ace.c
+++ src/libglobalscript/ace.c
@@ -125,8 +125,8 @@ ace_thread_pool_main(void *p)
         if (thread && thread->state == ace_thread_running) num_timeslots++;
         instr_start_time = gsflag_stat_collection ? nsec() : 0;
         if (thread) while (thread->state == ace_thread_running && nwork < 0x1000) {
-            if (ace_exec_efv(thread)) nwork++, num_instrs++;
-            else if (ace_exec_alloc(thread)) nwork++, num_instrs++;
+            while (ace_exec_efv(thread)) nwork++, num_instrs++;
+            if (ace_exec_alloc(thread)) nwork++, num_instrs++;
             else if (ace_exec_push(thread)) nwork++, num_instrs++;
             else if (ace_exec_branch(thread)) nwork++, num_instrs++;
             else if (ace_exec_terminal(thread)) nwork++, num_instrs++;
