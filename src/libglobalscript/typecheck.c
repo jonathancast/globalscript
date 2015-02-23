4eceddf2a091ec7a4e5c75fec3ff5d98786b1368
Jonathan C Cast <jcast@globalscript.org>
2015-02-23 18:23:03 +0000

    [types] Allow .tylets in among the arguments in a .impprog

--- src/libglobalscript/typecheck.c
+++ src/libglobalscript/typecheck.c
@@ -2936,6 +2936,7 @@ gsbc_typecheck_api_expr(struct gspos pos, struct gsfile_symtable *symtable, uint
 
     while (
         gsbc_typecheck_cast_op(p, &cl)
+        || gsbc_typecheck_code_type_let_op(p, &cl)
         || gsbc_typecheck_data_arg_op(p, &cl)
     )
         p = gsinput_next_line(ppseg, p)
